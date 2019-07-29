1a2a2d9bc466717423c94b7cba2c3ee4cbae5747
James King <james.king@facepunchstudios.com>
2019-07-29 16:04:53 +0100

    Add project files.

---
+++ Ants.Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ants.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
